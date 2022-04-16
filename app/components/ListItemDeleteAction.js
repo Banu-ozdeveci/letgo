4a91f45de30c10ee82e8ac8ccecaaebe385ca8bb
Banu Ozdeveci <ozdevecibanu@gmail.com>
2022-04-17 00:51:42 +0300

    added listItem

--- app/components/ListItemDeleteAction.js
+++ app/components/ListItemDeleteAction.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import {View,StyleSheet} from "react-native";
+import {View,StyleSheet,TouchableWithoutFeedback} from "react-native";
 import colors from '../config/colors';
 import {MaterialCommunityIcons} from "@expo/vector-icons"
 
-function ListItemDeleteAction(props) {
+function ListItemDeleteAction({onPress}) {
     return (
+        <TouchableWithoutFeedback onPress={onPress}>
+
      <View style={styles.container}>
          <MaterialCommunityIcons name="trash-can-outline" color="white" size={20}/></View>
+        </TouchableWithoutFeedback>
     );
 }
 
